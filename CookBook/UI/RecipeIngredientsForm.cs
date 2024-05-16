161c3cb05400ced3568c21b1850efdc4978c4760
codebeautyprogramming <codebeautyprogramming@gmail.com>
2024-05-16 15:29:38 +0200

    Startup code for this chapter

---
+++ CookBook/UI/RecipeIngredientsForm.cs
@@ -0,0 +1,44 @@
+﻿using DataAccessLayer.Contracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CookBook.UI
+{
+    public partial class RecipeIngredientsForm : Form
+    {
+        private readonly IRecipeIngredientsRepository _recipeIngredientsRepository;
+        private readonly IIngredientsRepository _ingredientsRepository;
+
+        public int RecipeId { get; set; }
+        public RecipeIngredientsForm(IRecipeIngredientsRepository recipeIngredientsReposiotry, IIngredientsRepository ingredientsRepository)
+        {
+            InitializeComponent();
+            _recipeIngredientsRepository = recipeIngredientsReposiotry;
+            _ingredientsRepository = ingredientsRepository;
+        }
+
+        private void RecipeIngredientsForm_Load(object sender, EventArgs e)
+        {
+            RefreshRecipeIngredients();
+            RefreshAllIngredients();
+        }
+
+        private async void RefreshRecipeIngredients()
+        {
+            RecipeIngredientsLbx.DataSource = await _recipeIngredientsRepository.GetRecipeIngredients(RecipeId);
+            RecipeIngredientsLbx.DisplayMember = "Name";
+        }
+        private async void RefreshAllIngredients()
+        {
+            AllIngredientsLbx.DataSource = await _ingredientsRepository.GetIngredients();
+            AllIngredientsLbx.DisplayMember = "Name";
+        }
+    }
+}
